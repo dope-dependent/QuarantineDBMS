3fda9d0eb366710fbbe10a8dfb89e92f3bd39c0b
Rajas Bhatt <rajas.bhatt@yahoo.com>
2020-06-21 19:07:45 +0530

    Updates

---
+++ precache-manifest.0318af0ce3f36522fba47338e1874a0f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d324f93c6bd604057d8254a4d166c80",
+    "url": "/QuarantineDBMS/index.html"
+  },
+  {
+    "revision": "0e44fddbecddb1b0f89d",
+    "url": "/QuarantineDBMS/static/css/main.9e820a23.chunk.css"
+  },
+  {
+    "revision": "71375a1001700a8e5e86",
+    "url": "/QuarantineDBMS/static/js/2.d4661c70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/QuarantineDBMS/static/js/2.d4661c70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e44fddbecddb1b0f89d",
+    "url": "/QuarantineDBMS/static/js/main.d5bf8f80.chunk.js"
+  },
+  {
+    "revision": "f9b8bf18967b8dc3aff7",
+    "url": "/QuarantineDBMS/static/js/runtime-main.a4b9f977.js"
+  },
+  {
+    "revision": "ce290a1b9aabbc0d6783b4fdd79249ed",
+    "url": "/QuarantineDBMS/static/media/SubjectPic0.ce290a1b.jpg"
+  },
+  {
+    "revision": "1ab691bca72020f4641a19a4d2d6d697",
+    "url": "/QuarantineDBMS/static/media/SubjectPic1.1ab691bc.jpg"
+  },
+  {
+    "revision": "498695be3eb7af265c295184e0fa4ae9",
+    "url": "/QuarantineDBMS/static/media/SubjectPic2.498695be.jpg"
+  },
+  {
+    "revision": "f64fc20ca03a6c88a738a2c60b010b07",
+    "url": "/QuarantineDBMS/static/media/SubjectPic3.f64fc20c.jpg"
+  },
+  {
+    "revision": "9fddd9aece86408da107783c4cd37b80",
+    "url": "/QuarantineDBMS/static/media/SubjectPic4.9fddd9ae.jpg"
+  },
+  {
+    "revision": "5b2a6fb3a6f58caeb5a4f61e84313c43",
+    "url": "/QuarantineDBMS/static/media/bg2.5b2a6fb3.jpg"
+  },
+  {
+    "revision": "42f3b079c67c4e367933a8979422b5a3",
+    "url": "/QuarantineDBMS/static/media/bg3.42f3b079.jpg"
+  },
+  {
+    "revision": "4e2921ae2aa6ab286dca55f6d6822395",
+    "url": "/QuarantineDBMS/static/media/linea-arrows-10.4e2921ae.ttf"
+  },
+  {
+    "revision": "839102e05032bae37b076c3c44c951b7",
+    "url": "/QuarantineDBMS/static/media/linea-arrows-10.839102e0.svg"
+  },
+  {
+    "revision": "9b287297a8d930c35c32a62fed4eba68",
+    "url": "/QuarantineDBMS/static/media/linea-arrows-10.9b287297.eot"
+  },
+  {
+    "revision": "e9c2f96041f9aa6fa05699a7e361d5cf",
+    "url": "/QuarantineDBMS/static/media/linea-arrows-10.e9c2f960.woff"
+  }
+]);
